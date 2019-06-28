ea577546a206e33b08dc4d133533e7431808115f
tdba <tanguy.debels@gmail.com>
2019-06-28 09:14:38 +0200

    Campaign scraping: title+holder+desc

--- scraper.py
+++ scraper.py
@@ -14,8 +14,11 @@ import pickle
 import campaign
 
 
+URL = 'https://www.kisskissbankbank.com'
+
+
 def max_page():
-    url = 'https://www.kisskissbankbank.com/fr/discover?filter=all'
+    url = URL + '/fr/discover?filter=all'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html5lib")
 
@@ -29,7 +32,7 @@ def links(max_p, current_page=1):
     res = []
 
     while current_page <= max_p:
-        url = f'https://www.kisskissbankbank.com/fr/discover?filter=all&page={current_page}'
+        url = URL + f'/fr/discover?filter=all&page={current_page}'
         response = requests.get(url)
 
         sleep(randint(8, 15))
@@ -56,19 +59,23 @@ def data_extraction(urls):
     res = []
 
     for url in urls:
-        response = requests.get(url)
+        response = requests.get(url + '/tabs/description')
         soup = BeautifulSoup(response.content, 'html5lib')
 
         title = soup.find('h1', attrs={'data-test-id': 'project-title'}).text
         desc = soup.find('head').find('meta', attrs={'name': 'description'})['content']
-        project_holder = soup.find('a', attrs={'class': 'k-u-size-tiny k-u-weight-regular kiss-Link--primary1'})['href']
+        project_holder = URL + soup.find('div', class_='owner-info__StyledOwner-sc-1r3rkij-2 gDZNgQ').a['href']
+        print(soup.head)
 
-        print(soup.prettify())
+    return res
 
 
 if __name__ == '__main__':
-    #max_page = max_page()
-    #campaign_links = links(max_page)
-    #with open('links', mode='wb') as f:
-    #    pickle.dump(campaign_links, f)
-    data_extraction(['https://www.kisskissbankbank.com/fr/projects/des-fourmis-dans-les-mains-un-grand-feu'])#links)
+    max_page = max_page()
+    campaign_links = links(max_page)
+    with open('links', 'wb') as f:
+        pickle.dump(campaign_links, f)
+
+    #with open('links', 'rb') as f:
+    #    campaign_links = pickle.
+    #data_extraction(['https://www.kisskissbankbank.com/fr/projects/timada-une-expedition-scientifique-etudiante-au-coeur-de-madagascar'])#campaign_links)
